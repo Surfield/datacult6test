dc1d0dc3273f9025ee8931dd4b954f4d0fb5b8f6
Surfield Thomas <SurfieldA@gmail.com>
2018-07-26 11:56:57 -0400

    test2

--- run.py
+++ run.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
+if __name__ == "__main__":
+    app.run()
+    # app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
